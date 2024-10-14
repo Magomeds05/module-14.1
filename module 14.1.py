8502ec5b89ead0d34183392f2e193132141897a4
Magomeds05 <yusypovm@gmail.com>
2024-10-14 18:00:37 +0300

    Update module 14.1.py

--- module 14.1.py
+++ module 14.1.py
@@ -19,25 +19,18 @@ for i in range(1, 11):
     curs.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", "1000"))
 
 #Обновите balance у каждой 2ой записи начиная с 1ой на 500:
-
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User1"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User3"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User5"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User7"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User9"))
+for j in range(1, 11, 2):
+    curs.execute("UPDATE Users SET balance = ? WHERE age = ? ", (500, j*10))
 
 #Удалите каждую 3ую запись в таблице начиная с 1ой:
-
-curs.execute("DELETE FROM Users WHERE username = ?", ("User1",))
-curs.execute("DELETE FROM Users WHERE username = ?", ("User4",))
-curs.execute("DELETE FROM Users WHERE username = ?", ("User7",))
-curs.execute("DELETE FROM Users WHERE username = ?", ("User10",))
+for k in range(1, 11, 3):
+    curs.execute("DELETE FROM Users WHERE username = ?", (k,))
 
 #Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60 и выведите их в консоль в следующем формате (без id):
-curs.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+curs.execute("SELECT * FROM Users WHERE age != 60")
 users = curs.fetchall()
-for j in users:
-    print(j)
+for l in users:
+    print(f'Имя: {l[1]} | Почта: {l[2]} | Возраст: {l[3]} | Баланс: {l[4]}')
 
 connection.commit()
 connection.close()
